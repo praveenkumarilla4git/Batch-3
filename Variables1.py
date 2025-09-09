1dc2c564dd73b05dab838dbe2fde8d2cda035037
praveenkumarila4git <praveenkumarilla@gmail.com>
2025-09-09 11:13:00 +0530

    1. Variables Class

---
+++ Variables1.py
@@ -0,0 +1,36 @@
+# Variables are of different data types like Integer, float, String
+a = 10 #Integer
+b = 3.5 # decimal (float)
+name = "Aruna" # text (string)
+
+print(f"value of variable a is: {a}")
+#print(a)
+#print(a)
+
+x = 100
+#print(x)
+
+x = 200
+print(x)
+
+a = 5
+b = 7
+sum = a+b
+print("Sum is:", sum)
+print(f"Sum is: {sum}")
+
+#Rules for Variables:
+# 1. Must start with a letter or underscore
+# 2. We can use letters, numbers and underscores
+# 3. class --> system defined key word & def --> system defined key word
+# We cannot use System defined keywords as Variables.
+
+#name1 = "Hari" # Valid
+#_name = "SHIVA" # Valid
+#1name = "Brahma" #Invalid
+
+name = "Praveen"
+age = 39
+height = 5.7
+
+print("My name is",name, ", I am ", age, "years old and my height is", height)
