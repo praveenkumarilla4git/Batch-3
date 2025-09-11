bc35cb905881e17c414416851551e03def69fa51
praveenkumarila4git <praveenkumarilla@gmail.com>
2025-09-11 10:29:58 +0530

    added all files to feature1

--- data_type_print.py
+++ data_type_print.py
@@ -7,3 +7,16 @@ is_static = True
 print(90)
 print(x)
 print(type(is_static))
+a = 10
+b = 3.14
+c = "Hello"
+d = True
+e = 'Hello'
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print("Hello")
+print('Hello')
